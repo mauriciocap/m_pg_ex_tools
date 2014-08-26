88cbb711c45328107cdc38e5f47da43975e452bb
mauriciocap@gmail.com <mauriciocap@gmail.com>
2014-08-26 12:05:12 -0300

    TRY: arrancar app del mapa

--- 0init.js
+++ 0init.js
@@ -1,1 +1,7 @@
-alert("hola de github");
+function runApp() {
+	var s0= function () { getHttpToDflt('app.js','http://192.168.10.8:8080/www/app.js',s1,s1); }
+	var s1= function () { evalFileOrDflt('app.js',false,nullf); }
+	s0();
+}
+
+runApp();
