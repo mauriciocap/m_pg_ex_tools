a3c6805dcba1c96af14eb67ac99667caba38a1cc
mauriciocap@gmail.com <mauriciocap@gmail.com>
2014-08-26 23:59:56 -0300

    TRY: init screen with normal / dbg options

--- 0init.js
+++ 0init.js
@@ -24,8 +24,14 @@ function getHttpToDflt(fname,url,cbok,cbfail) {
 
 function runApp() {
 	var s0= function () { getHttpToDflt('app.js','http://192.168.10.8:8080/www/app.js',s1,s1); }
-	var s1= function () { CFGLIB.loglvlmax=3; evalFileOrDflt('app.js',false,nullf); }
+	var s1= function () { evalFileOrDflt('app.js',false,nullf); }
 	s0();
 }
 
-runApp();
+function initScreen() {
+	$(document.body).html('<p><button id="btnRun" value="RUN"><p><button id="btnRunDbg" value="DBG">');
+	$('#btnRun').on('click',function () { CFGLIB.loglvlmax=0; runApp(); }
+	$('#btnRunDbg').on('click',function () { CFGLIB.loglvlmax=9; runApp(); }
+}
+
+initScreen();
